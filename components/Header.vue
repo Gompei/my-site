86ed37100046e85e2ba101cafd5dcc1a438138bb
Gompei <shoueigonpei6028@gmail.com>
2021-10-23 16:56:02 +0900

    リンク修正

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,20 @@
 <template>
   <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <nuxt-link to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
         <img src="~/assets/image/icon.png" class="logo-image">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
         </svg>
         <span class="ml-3 text-xl">Gompei Blog</span>
-      </a>
+      </nuxt-link>
       <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
-        <nuxt-link class="mr-5 hover:text-gray-900" to="/">
+        <nuxt-link to="/" class="mr-5 hover:text-gray-900">
           home
         </nuxt-link>
-        <nuxt-link class="mr-5 hover:text-gray-900" to="/about">
+        <nuxt-link to="/about" class="mr-5 hover:text-gray-900">
           about
         </nuxt-link>
-        <a class="mr-5 hover:text-gray-900" href="https://github.com/Gompei">github</a>
+        <a href="https://github.com/Gompei" class="mr-5 hover:text-gray-900">github</a>
       </nav>
     </div>
   </header>
