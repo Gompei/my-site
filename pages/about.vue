fb2857f81f01f28193610192da9caad256557ea1
Gompei <shoueigonpei6028@gmail.com>
2021-10-23 18:20:56 +0900

    aboutページ修正

--- pages/about.vue
+++ pages/about.vue
@@ -1,303 +1,55 @@
 <template>
   <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-col text-center w-full mb-20">
-        <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">
-          <font style="vertical-align: inherit;">
-            <font style="vertical-align: inherit;">
-              私たちのチーム
-            </font>
-          </font>
+    <div class="container px-5 pt-10 mx-auto">
+      <div class="text-center mb-2">
+        <img src="~/assets/image/icon.png" class="logo-image">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-2">
+          Gompei
         </h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-          <font style="vertical-align: inherit;">
-            <font style="vertical-align: inherit;">
-              カーディガンのトートバッグタンブラー六角形ブルックリン非対称ジェントリフィケーション、地下鉄タイルポークファームツーテーブル。
-            </font>
-            <font style="vertical-align: inherit;">
-              フランツェンあなたはおそらくそれらのことを聞いたことがないでしょう。
-            </font>
-          </font>
+        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+          バックエンド・クラウドエンジニア
         </p>
+        <div class="flex mt-2 justify-center">
+          <div class="w-32 h-1 rounded-full bg-indigo-500 inline-flex" />
+        </div>
       </div>
-      <div class="flex flex-wrap -m-4">
-        <div class="p-4 lg:w-1/4 md:w-1/2">
-          <div class="h-full flex flex-col items-center text-center">
-            <img alt="チーム" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/200x200">
-            <div class="w-full">
-              <h2 class="title-font font-medium text-lg text-gray-900">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    アルパーカム
-                  </font>
-                </font>
+    </div>
+    <div class="container px-5 pt-5 mx-auto">
+      <div class="text-center">
+        <h3 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
+          経歴
+        </h3>
+      </div>
+    </div>
+    <div class="container mx-auto flex flex-col">
+      <div class="lg:w-4/6 mx-auto">
+        <div class="flex flex-col sm:flex-row mt-10">
+          <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+            <div class="flex flex-col items-center text-center justify-center">
+              <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
+                BACKEND
               </h2>
-              <h3 class="text-gray-500 mb-3">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    UI開発者
-                  </font>
-                </font>
-              </h3>
-              <p class="mb-4">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    酢クロナッツアダプトゲンイカファニーパックベーパーウェアを飲むDIYトートバッグ。
-                  </font>
-                </font>
+              <div class="w-20 h-1 bg-indigo-500 rounded mt-2 mb-4" />
+              <p class="text-base">
+                Go・C#・Java
               </p>
-              <span class="inline-flex">
-                <a class="text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                  </svg>
-                </a>
-              </span>
             </div>
           </div>
-        </div>
-        <div class="p-4 lg:w-1/4 md:w-1/2">
-          <div class="h-full flex flex-col items-center text-center">
-            <img alt="チーム" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/201x201">
-            <div class="w-full">
-              <h2 class="title-font font-medium text-lg text-gray-900">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    ホールデンコールフィールド
-                  </font>
-                </font>
-              </h2>
-              <h3 class="text-gray-500 mb-3">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    UI開発者
-                  </font>
-                </font>
-              </h3>
-              <p class="mb-4">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    酢クロナッツアダプトゲンイカファニーパックベーパーウェアを飲むDIYトートバッグ。
-                  </font>
-                </font>
-              </p>
-              <span class="inline-flex">
-                <a class="text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                  </svg>
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 lg:w-1/4 md:w-1/2">
-          <div class="h-full flex flex-col items-center text-center">
-            <img alt="チーム" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/202x202">
-            <div class="w-full">
-              <h2 class="title-font font-medium text-lg text-gray-900">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    アティカス・フィンチ
-                  </font>
-                </font>
-              </h2>
-              <h3 class="text-gray-500 mb-3">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    UI開発者
-                  </font>
-                </font>
-              </h3>
-              <p class="mb-4">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    酢クロナッツアダプトゲンイカファニーパックベーパーウェアを飲むDIYトートバッグ。
-                  </font>
-                </font>
-              </p>
-              <span class="inline-flex">
-                <a class="text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                  </svg>
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 lg:w-1/4 md:w-1/2">
-          <div class="h-full flex flex-col items-center text-center">
-            <img alt="チーム" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/203x203">
-            <div class="w-full">
-              <h2 class="title-font font-medium text-lg text-gray-900">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    ヘンリー・レザム
-                  </font>
-                </font>
-              </h2>
-              <h3 class="text-gray-500 mb-3">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    UI開発者
-                  </font>
-                </font>
-              </h3>
-              <p class="mb-4">
-                <font style="vertical-align: inherit;">
-                  <font style="vertical-align: inherit;">
-                    酢クロナッツアダプトゲンイカファニーパックベーパーウェアを飲むDIYトートバッグ。
-                  </font>
-                </font>
-              </p>
-              <span class="inline-flex">
-                <a class="text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-                  </svg>
-                </a>
-                <a class="ml-2 text-gray-500">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                  </svg>
-                </a>
-              </span>
-            </div>
+          <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+            <p class="leading-relaxed text-lg mb-4">
+              テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。
+            </p>
           </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.logo-image {
+  width: 52px;
+  height: 55px;
+  margin: 0 auto 10px auto;
+}
+</style>
