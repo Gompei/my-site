94edc3e2dc838527b824616f388f4fbd828773e8
Gompei <shoueigonpei6028@gmail.com>
2021-10-23 16:49:52 +0900

    フッター作成

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="text-gray-600 body-font">
+    <div class="px-5 py-7 mx-auto">
+      <p class="text-sm text-gray-500 text-center">
+        © 2021 Gompei / Shoei Gompei
+      </p>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <Header />
     <nuxt />
+    <Footer />
   </div>
 </template>
 
