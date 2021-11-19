b325fe02bf28361ff2e10942f7e819695bbc8f02
Gompei <shoueigonpei6028@gmail.com>
2021-11-19 20:21:27 +0900

    image alt追加

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
   <header class="text-gray-600 body-font">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <nuxt-link to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <img src="~/assets/image/log.png" class="logo-image">
+        <img alt="gompei-blog-logo" src="~/assets/image/log.png" class="logo-image">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
         </svg>
         <span class="ml-3 text-xl">Gompei Blog</span>
