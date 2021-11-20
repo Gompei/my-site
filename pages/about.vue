eaed2a3159c0b35358625d8f25edf47f2ee475f3
Gompei <shoueigonpei6028@gmail.com>
2021-11-20 21:42:06 +0900

    aboutページ修正

--- pages/about.vue
+++ pages/about.vue
@@ -1,44 +1,275 @@
 <template>
   <section class="text-gray-600 body-font">
-    <div class="container px-5 pt-10 mx-auto">
+    <div class="container px-5 pt-5 mx-auto">
       <div class="text-center mb-2">
         <img src="~/assets/image/icon.png" class="logo-image">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-2">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
           Gompei
         </h1>
         <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
-          エンジニア
+          Backend Engineer
         </p>
-        <div class="flex mt-2 justify-center">
-          <div class="w-32 h-1 rounded-full bg-indigo-500 inline-flex" />
+        <div class="flex justify-center">
+          <div class="w-32 line rounded-full bg-indigo-500 inline-flex" />
         </div>
       </div>
     </div>
-    <div class="container px-5 pt-5 mx-auto">
-      <div class="text-center">
-        <h3 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">
-          経歴
-        </h3>
+    <div>
+      <div class="container pt-5 mx-auto">
+        <div class="text-center">
+          <p class="sm:text-xl text-2xl font-medium text-gray-900">
+            経歴
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="container mx-auto flex flex-col">
-      <div class="lg:w-4/6 mx-auto">
-        <div class="flex flex-col sm:flex-row mt-10">
-          <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-            <div class="flex flex-col items-center text-center justify-center">
-              <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">
-                BACKEND
-              </h2>
-              <div class="w-20 h-1 bg-indigo-500 rounded mt-2 mb-4" />
-              <p class="text-base">
-                Go・C#・Java
-              </p>
+      <div class="container mx-auto flex flex-col">
+        <div class="lg:w-4/6 mx-auto">
+          <div class="flex flex-col sm:flex-row mt-2">
+            <div class="sm:w-1/3 text-center sm:pr-4 sm:py-4">
+              <div class="flex flex-col items-center text-center justify-center">
+                <h2 class="font-medium title-font mt-4 text-gray-900 text-base">
+                  CLOUD
+                </h2>
+                <div class="w-20 line bg-indigo-500 rounded mb-2 mt-1" />
+                <p class="text-base">
+                  AWS
+                </p>
+                <p class="text-base">
+                  GCP
+                </p>
+                <p class="text-base">
+                  Azure
+                </p>
+              </div>
+              <div class="flex flex-col items-center text-center justify-center">
+                <h2 class="font-medium title-font mt-4 text-gray-900 text-base">
+                  IaC
+                </h2>
+                <div class="w-20 line bg-indigo-500 rounded mb-2 mt-1" />
+                <p class="text-base">
+                  Terraform
+                </p>
+                <p class="text-base">
+                  CloudFomrtion
+                </p>
+                <p class="text-base">
+                  mitamae
+                </p>
+              </div>
+              <div class="flex flex-col items-center text-center justify-center">
+                <h2 class="font-medium title-font mt-4 text-gray-900 text-base">
+                  BACKEND
+                </h2>
+                <div class="w-20 line bg-indigo-500 rounded mb-2 mt-1" />
+                <p class="text-base">
+                  Go
+                </p>
+                <p class="text-base">
+                  C#
+                </p>
+                <p class="text-base">
+                  Java
+                </p>
+              </div>
+            </div>
+            <div class="sm:w-2/3 sm:pl-4 sm:py-4 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+              <p class="leading-relaxed text-lg mb-4" />
             </div>
           </div>
-          <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-            <p class="leading-relaxed text-lg mb-4">
-              テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。テスト。
-            </p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="container pt-5 mx-auto">
+        <div class="text-center">
+          <p class="sm:text-xl text-2xl font-medium text-gray-900">
+            OSS
+          </p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="container pt-5 mx-auto">
+        <div class="text-center">
+          <p class="sm:text-xl text-2xl font-medium text-gray-900">
+            Webアプリ
+          </p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="container pt-5 mx-auto">
+        <div class="text-center">
+          <p class="sm:text-xl text-2xl font-medium text-gray-900">
+            資格
+          </p>
+        </div>
+      </div>
+      <div>
+        <div class="w-full max-w-screen-xl mx-auto">
+          <div class="flex justify-center p-4 py-2">
+            <div class="w-full max-w-md">
+              <div class="bg-white">
+                <div class="py-3 text-sm">
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="" target="_blank">
+                        2021年11月
+                        Microsoft Certified：Azure administrator associate
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="" target="_blank">
+                        2021年11月
+                        Google Cloud Certified - Professional Cloud Architect
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/9eee9bc3-e314-4551-9581-dc6c731a028c/public_url" target="_blank">
+                        2021年10月
+                        Certified Kubernetes Administrator
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/222dddf7-56b7-40cb-b084-5827db945969/public_url" target="_blank">
+                        2021年9月
+                        AWS Certified Cloud Practitioner
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      2021年9月
+                      LPIC2
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/b1da38a7-2f3b-48eb-bb7e-38fe470a0bdf/public_url" target="_blank">
+                        2021年8月
+                        AWS Certified Security – Specialty
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/4a9161ed-6f06-4c2d-b6ea-a3290987aeee/public_url" target="_blank">
+                        2021年7月
+                        AWS Certified Advanced Networking – Specialty
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/89c28e3f-2d09-48b6-8d6c-08ccf2916661/public_url" target="_blank">
+                        2021年7月
+                        Microsoft Certified：Azure Fundamentals
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/3bc6d162-efaf-4b7b-9a0f-9da157680fcc/public_url" target="_blank">
+                        2021年6月
+                        AWS Certified Solutions Architect – Professional
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      2021年6月
+                      OSS-DB Silver
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/a5d7976b-d7de-40fe-a1b3-15b647a536a5/public_url" target="_blank">
+                        2021年5月
+                        CCNA
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/e86d2e0d-1f77-4953-92d4-a87de88f96b8/public_url" target="_blank">
+                        2021年3月
+                        AWS Certified Solutions Architect – Associate
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/a5f621d2-8d63-4042-9575-791a18f48f66/public_url" target="_blank">
+                        2020年9月
+                        ORACLE MASTER Bronze Certified Oracle Database 12c - JPN
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      2020年8月
+                      LPIC1
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="" target="_blank">
+                        2020年7月
+                        Google Cloud Certified - Associate Cloud Engineer
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/1840a046-4b8c-4b20-b389-b92610698aac/public_url" target="_blank">
+                        2020年5月
+                        Oracle Certified Java Programmer, Gold SE 8
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/cf68371f-22e4-4a09-b550-19470b25e294/public_url" target="_blank">
+                        2020年2月
+                        Oracle Certified Java Programmer, Silver SE 8
+                      </a>
+                    </div>
+                  </div>
+                  <div class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                    <span class="bg-indigo-500 h-2 w-2 m-2 rounded-full" />
+                    <div class="flex-grow font-medium px-2">
+                      <a title="" href="https://www.credly.com/badges/efcc9057-6d90-4ec0-ac2f-e098f37518d5/public_url" target="_blank">
+                        2020年2月
+                        Oracle Certified Java Programmer, Bronze SE 7/8
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -47,9 +278,13 @@
 </template>
 
 <style scoped>
+.line {
+  height: 0.1px;
+}
+
 .logo-image {
-  width: 52px;
-  height: 55px;
-  margin: 0 auto 10px auto;
+  width: 62px;
+  height: 65px;
+  margin: 0 auto 5px auto;
 }
 </style>
