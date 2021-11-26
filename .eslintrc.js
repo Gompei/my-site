a9d48fffda74659397a60445f21074e620164514
Gompei <shoueigonpei6028@gmail.com>
2021-11-26 09:50:27 +0900

    eslint vue/no-v-htmlルール追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'vue/multi-word-component-names': 'off',
-    'vue/attribute-hyphenation': 'off'
+    'vue/attribute-hyphenation': 'off',
+    'vue/no-v-html': 'off'
   }
 }
