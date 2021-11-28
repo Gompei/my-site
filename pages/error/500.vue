2a198f301b8756a3dcbffdb6b8c2e03bfca83773
Gompei <shoueigonpei6028@gmail.com>
2021-11-28 11:34:27 +0900

    500エラー追加

---
+++ pages/error/500.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my-4 w-screen flex items-center">
+    <div class="container flex flex-col md:flex-row items-center justify-center text-gray-700">
+      <div class="text-center max-w-md">
+        <div class="text-5xl font-dark font-bold">
+          500
+        </div>
+        <p class="text-2xl md:text-3xl font-dark font-bold">
+          InternalServerError
+        </p>
+      </div>
+      <div>
+        <img src="~/assets/image/error_icon.png" class="error-image" alt="error icon">
+      </div>
+    </div>
+  </div>
+</template>
