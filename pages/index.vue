45d626a84cb0ec34bdc6d3924c1a81e0b6c51eed
Gompei <shoueigonpei6028@gmail.com>
2021-11-29 20:03:08 +0900

    index.vue作業保存

--- pages/index.vue
+++ pages/index.vue
@@ -13,35 +13,14 @@ import { Component, Vue } from 'nuxt-property-decorator'
 import { Context } from '@nuxt/types'
 import { Article } from '@/types'
 
-@Component({
-  watchQuery: ['page']
-})
+@Component
 export default class Index extends Vue {
   private articles: Article[] = []
   private total: number = 0
   private nowPage: number = 1
 
-  asyncData ({ query }: Context) {
-    console.log(typeof query)
-    console.log(query.page)
-    //  const page = parseInt(query.page);
-    // this.nowPage = !query.page ? 1 : parseInt(query.page)
-
-    // TODO: テストデータ
-    const articles: Article[] = []
-    for (let i = 1; i <= 8; i++) {
-      articles.push({
-        id: i,
-        title: 'テストタイトル' + i,
-        subTitle: 'テストサブタイトル' + i,
-        categoryTag: 'テストタグ' + i
-      })
-    }
-
-    return {
-      articles,
-      total: 80
-    }
+  asyncData ({ $config }: Context) {
+    return {}
   }
 }
 </script>
