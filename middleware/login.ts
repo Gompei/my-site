b3582d416dd9ab280a7451afa7b95f7b520ddfc4
Gompei <shoueigonpei6028@gmail.com>
2021-12-03 19:45:45 +0900

    ミドルウェア修正

--- middleware/login.ts
+++ middleware/login.ts
@@ -1,10 +1,21 @@
 import { Middleware, Context } from '@nuxt/types'
+import { CognitoUser, CognitoUserSession } from 'amazon-cognito-identity-js'
 
 const loginList = ['/article/edit']
 
 const login: Middleware = (context: Context) => {
   if (loginList.includes(context.route.path)) {
-    context.redirect('/')
+    const cognitoUser: CognitoUser | null = context.app.context.$cognitoUserPool.getCurrentUser()
+
+    if (cognitoUser !== null) {
+      cognitoUser.getSession((err: Error | null, _session : CognitoUserSession) => {
+        if (err !== null) {
+          context.redirect('/')
+        }
+      })
+    } else {
+      context.redirect('/')
+    }
   }
 }
 
