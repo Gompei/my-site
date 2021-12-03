f15bf35dbe54c2f43fccc1b5da60c7b68452732b
Gompei <shoueigonpei6028@gmail.com>
2021-12-03 19:45:06 +0900

    Articles.vue一時修正
    後で戻す

--- components/Articles.vue
+++ components/Articles.vue
@@ -7,13 +7,12 @@
         </h1>
       </div>
     </div>
-
     <div>
       <div class="container">
         <div class="flex flex-wrap place-items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <div v-for="(article, index) in articles" :key="index" class="p-1 flex-1">
+          <div v-for="(article, i) in articles" :key="i" class="p-1 flex-1">
             <div class="overflow-hidden shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-5 hover:shadow-2xl rounded-lg h-90 w-72 cursor-pointer m-2">
-              <nuxt-link to="/article">
+              <nuxt-link :to="{ name: 'article-id', params: { id: article.id }}">
                 <img alt="blog photo" src="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80" class="max-h-40 w-full object-cover">
                 <div class="bg-white p-4">
                   <p class="text-indigo-500 text-2xl font-medium">
@@ -23,14 +22,15 @@
                     {{ article.subTitle }}
                   </p>
                   <div class="flex flex-wrap justify-starts items-center py-3 border-b-2 text-xs text-white font-medium">
-                    <span class="m-1 px-2 py-1 rounded bg-indigo-500">
-                      #タグ
+                    <span v-for="(tag, j) in article.categoryTag" :key="j" class="m-1 px-2 py-1 rounded bg-indigo-500">
+                      #{{ tag }}
+                      {{ j }}
                     </span>
                   </div>
                   <div class="flex items-center mt-2">
                     <div class="pl-3">
                       <div class="font-medium">
-                        YYYY年MM月DD日
+                        {{ article.create_time_stamp }}
                       </div>
                     </div>
                   </div>
@@ -40,55 +40,6 @@
           </div>
         </div>
       </div>
-
-      <!-- <div class="flex flex-col items-center my-12">
-        <div class="flex text-gray-700">
-          <div class="h-8 w-8 mr-1 flex justify-center items-center  cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="100%"
-              height="100%"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-chevron-left w-4 h-4"
-            >
-              <polyline points="15 18 9 12 15 6" />
-            </svg>
-          </div>
-          <div class="flex h-8 font-medium ">
-            <nuxt-link
-              v-for="page of pageTotal"
-              :key="page"
-              :to="{path: `/?page=${page}`}"
-              class="w-8 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in"
-              :class="page === nowPage ? border-t-2 : border-t-0"
-              @click.native="transitionPage"
-            >
-              {{ page }}
-            </nuxt-link>
-          </div>
-          <div class="h-8 w-8 ml-1 flex justify-center items-center  cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="100%"
-              height="100%"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="feather feather-chevron-right w-4 h-4"
-            >
-              <polyline points="9 18 15 12 9 6" />
-            </svg>
-          </div>
-        </div>
-      </div> -->
     </div>
   </div>
 </template>
@@ -99,23 +50,11 @@ import { Article } from '@/types'
 
 @Component
 export default class Articles extends Vue {
-    @Prop({ type: Number, required: true, default: 1 })
-      nowPage!: number
-
     @Prop({ type: [Object, Array], required: true, default: [] })
       articles!: Article[]
 
-    @Prop({ type: Number, required: true, default: 0 })
-      total!: number
-
-    private pageTotal : number | undefined
-
-    created () {
-      this.calculatePageTotal()
-    }
-
-    calculatePageTotal () {
-      this.pageTotal = this.total / 8
+    mounted () {
+      console.log(this.articles)
     }
 }
 </script>
