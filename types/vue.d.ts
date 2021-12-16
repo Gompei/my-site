5ac8446f6f785ab5a1b40044d017c5775f0f0fc0
Gompei <shoueigonpei6028@gmail.com>
2021-12-16 16:40:00 +0900

    cognito.ts削除

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -1,10 +1,8 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js'
 import { Repositories } from '@/plugins/repositoryFactory'
 
 declare module 'vue/types/vue' {
     interface Vue {
       $repositories: Repositories
-      $cognito: CognitoUserPool
     }
 }
 
